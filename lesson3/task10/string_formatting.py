d6ffd7276d94e7c8f37cfd29f8f2fad5b98f36bd
aleharlou <aleh.arlou@gmail.com>
2016-05-05 00:50:07 +0300

    test

---
+++ lesson3/task10/string_formatting.py
@@ -0,0 +1,6 @@
+name = "John"
+years = 30
+
+print("Hello, PyCharm! My name is %s!" % name)     # Note: %s is inside the string, % is after the string
+
+print("I'm %d years old" % years)
---
+++ lesson4/task1/lists.py
@@ -0,0 +1,5 @@
+squares = [1, 4, 9, 16, 25]   # create new list
+print(squares)
+
+print(slice)
+123
